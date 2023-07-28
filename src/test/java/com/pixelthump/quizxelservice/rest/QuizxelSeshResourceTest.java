ab74fdf1d088f17ccf614280bb11d8250825ad0f
roboter5123 <pascal.roboter@hotmail.com>
2023-07-28 21:35:55 +0200

    Fixed tests in resource

--- src/test/java/com/pixelthump/quizxelservice/rest/QuizxelSeshResourceTest.java
+++ src/test/java/com/pixelthump/quizxelservice/rest/QuizxelSeshResourceTest.java
@@ -2,45 +2,50 @@ package com.pixelthump.quizxelservice.rest;
 import com.pixelthump.quizxelservice.Application;
 import com.pixelthump.quizxelservice.rest.model.QuizxelSeshInfo;
 import com.pixelthump.quizxelservice.service.SeshService;
-import org.junit.jupiter.api.AfterEach;
+import com.pixelthump.quizxelservice.service.model.SeshInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = Application.class)
 class QuizxelSeshResourceTest {
 
     @Autowired
     QuizxelSeshResource seshResource;
-    @Autowired
+    @MockBean
     SeshService seshService;
     private final String existingSeshCode = "abcd";
     private final String nonExistingSeshCode = "efgh";
 
+    private SeshInfo existingSesh;
+    private SeshInfo nonExistingSesh;
+
     @BeforeEach
     public void setup() {
 
-        seshService.hostSesh(existingSeshCode);
+        existingSesh = new SeshInfo();
+        existingSesh.setSeshType("QUIZXEL");
+        existingSesh.setSeshCode(existingSeshCode);
+
+        nonExistingSesh = new SeshInfo();
+        nonExistingSesh.setSeshType("QUIZXEL");
+        nonExistingSesh.setSeshCode(nonExistingSeshCode);
     }
 
-    @AfterEach
-    public void tearDown() throws NoSuchFieldException, IllegalAccessException {
-
-        Field field = seshService.getClass().getDeclaredField("seshs");
-        field.setAccessible(true);
-        field.set(seshService, new HashMap<>());
-    }
 
     @Test
     void hostSeshInfo_nonexistingSeshCode_shouldReturnCorrectInfoAndHostSesh() {
+
+        when(seshService.hostSesh(any())).thenReturn(nonExistingSesh);
 
         QuizxelSeshInfo result = seshResource.hostSesh(nonExistingSeshCode);
 
@@ -54,11 +59,13 @@ class QuizxelSeshResourceTest {
     @Test
     void hostSeshInfo_existingSeshCode_shouldThrowCorrectException() {
 
+        String responseMessage = "Sesh with seshCode " + existingSeshCode + " already exists";
+        when(seshService.hostSesh(any())).thenThrow(new ResponseStatusException(HttpStatus.CONFLICT, responseMessage));
+
         ResponseStatusException result = assertThrows(ResponseStatusException.class, () -> seshResource.hostSesh(existingSeshCode));
 
-        String expectedMessage = "Sesh with seshCode " + existingSeshCode + " already exists";
         System.out.println(result.getMessage());
-        assertTrue(result.getMessage().contains(expectedMessage));
+        assertTrue(result.getMessage().contains(responseMessage));
 
         HttpStatusCode expectedStatusCode = HttpStatusCode.valueOf(409);
         assertEquals(expectedStatusCode, result.getStatusCode());
@@ -66,6 +73,8 @@ class QuizxelSeshResourceTest {
 
     @Test
     void getSeshInfo_existingSesh_shouldReturnCorrectInfo() {
+
+        when(seshService.getSeshInfo(any())).thenReturn(existingSesh);
 
         QuizxelSeshInfo result = seshResource.getSeshInfo(existingSeshCode);
 
@@ -79,10 +88,11 @@ class QuizxelSeshResourceTest {
     @Test
     void getSeshInfo_nonExistingSesh_shouldThrowCorrectException() {
 
+        String responseMessage = "Sesh with seshCode " + nonExistingSeshCode + " not found";
+        when(seshService.getSeshInfo(any())).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, responseMessage));
         ResponseStatusException result = assertThrows(ResponseStatusException.class, () -> seshResource.getSeshInfo(nonExistingSeshCode));
 
-        String expectedMessage = "Sesh with seshCode " + nonExistingSeshCode + " not found";
-        assertTrue(result.getMessage().contains(expectedMessage));
+        assertTrue(result.getMessage().contains(responseMessage));
 
         HttpStatusCode expectedStatusCode = HttpStatusCode.valueOf(404);
         assertEquals(expectedStatusCode, result.getStatusCode());
