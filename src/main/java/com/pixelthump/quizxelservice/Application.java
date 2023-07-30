666dcdf1cfbdd51fd6ed3f33ce891a2510bc3f85
roboter5123 <pascal.roboter@hotmail.com>
2023-07-30 16:41:28 +0200

    Actualy enabled scheduling

--- src/main/java/com/pixelthump/quizxelservice/Application.java
+++ src/main/java/com/pixelthump/quizxelservice/Application.java
@@ -1,8 +1,10 @@
 package com.pixelthump.quizxelservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
