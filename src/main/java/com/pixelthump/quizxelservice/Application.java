3cad1ab33eec433bd46ded5933aa64761bf50602
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-08-08 14:01:05 +0200

    Update Application.java

--- src/main/java/com/pixelthump/quizxelservice/Application.java
+++ src/main/java/com/pixelthump/quizxelservice/Application.java
@@ -3,9 +3,11 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
 @EnableEncryptableProperties
 public class Application {
 
