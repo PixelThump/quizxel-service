cc0449097484def13ae5a55210e350f945ae1213
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-08-08 13:19:11 +0200

    Add files via upload

--- src/main/java/com/pixelthump/quizxelservice/config/BroadcastServiceConfig.java
+++ src/main/java/com/pixelthump/quizxelservice/config/BroadcastServiceConfig.java
@@ -2,25 +2,27 @@ package com.pixelthump.quizxelservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import com.pixelthump.quizxelservice.service.BroadcastService;
 import com.pixelthump.quizxelservice.service.StompBroadcastService;
 
+import lombok.extern.log4j.Log4j2;
+
 @Configuration
+@Log4j2
 public class BroadcastServiceConfig {
 
 	@Value("${pixelthump.backend-basepath}")
 	private String backendBaseUrl;
 
-	@Bean
 	@Autowired
 	BroadcastService StompBroadcastService(StompBroadcastService broadcastService, WebSocketStompClient stompClient) {
 
 		String url = backendBaseUrl.replace("https", "wss://");
 		url = url.replace("http", "ws://") + "/ws";
+		log.error(url);
 		stompClient.connectAsync(url, broadcastService);
 		return broadcastService;
 	}
