9ada676e41693beda12c22d60d67b0d43cedfe3c
roboter5123 <pascal.roboter@hotmail.com>
2023-08-20 09:56:59 +0200

    Added an endpoint for getting a players state

--- src/main/java/com/pixelthump/quizxelservice/service/JoinService.java
+++ src/main/java/com/pixelthump/quizxelservice/service/JoinService.java
@@ -1,5 +1,6 @@
 package com.pixelthump.quizxelservice.service;
 import com.pixelthump.quizxelservice.repository.model.player.Player;
+import com.pixelthump.quizxelservice.service.model.state.AbstractServiceState;
 import com.pixelthump.quizxelservice.service.model.state.controller.AbstractControllerState;
 import com.pixelthump.quizxelservice.service.model.state.host.AbstractHostState;
 
@@ -7,4 +8,6 @@ public interface JoinService {
 
     AbstractControllerState joinAsController(String seshCode, Player player);
     AbstractHostState joinAsHost(String seshCode);
+
+    AbstractServiceState getStateForPlayer(String seshCode, String playerName);
 }
