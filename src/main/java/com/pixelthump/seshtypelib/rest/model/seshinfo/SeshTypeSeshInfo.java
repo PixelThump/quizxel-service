159235942e2095792f57ee204165350f54f11593
roboter5123 <pascal.roboter@hotmail.com>
2023-08-27 11:07:31 +0200

    Removed seshtypelib package in favor for maven dependency

--- src/main/java/com/pixelthump/seshtypelib/rest/model/seshinfo/SeshTypeSeshInfo.java
+++
@@ -1,9 +0,0 @@
-package com.pixelthump.seshtypelib.rest.model.seshinfo;
-import lombok.Data;
-
-@Data
-public class SeshTypeSeshInfo {
-
-    private String seshType;
-    private String seshCode;
-}
--- src/main/java/com/pixelthump/seshtypelib/service/GameLogicService.java
+++
@@ -1,7 +0,0 @@
-package com.pixelthump.seshtypelib.service;
-import com.pixelthump.seshtypelib.service.model.State;
-
-public interface GameLogicService {
-
-    State processQueue(State state);
-}
